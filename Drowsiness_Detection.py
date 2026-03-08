5e0e540b95b71a1e3c26aea4346174df9514281e
venkat-karthik <karthikkodeboyina@gmail.com>
2026-03-08 13:32:00 +0530

    Set up facial landmark indices for eye tracking

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -18,3 +18,7 @@ thresh = 0.25
 frame_check = 20
 detect = dlib.get_frontal_face_detector()
 predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")  # Dat file is the crux of the code
+
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
